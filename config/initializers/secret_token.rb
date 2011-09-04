8b1e7978ff58660198f8cae691f9138d5d894a44
Sergey Hanchar <hanchar.sergey@gmail.com>
2011-09-04 09:11:32 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FindMeGps::Application.config.secret_token = '0128a9ee5ee52dc2bb07f4f1f05b727fda9db8cea7d9f71729e7ff6466ac4e0682b63863825fdbbd2e9119a4527616201b2827af9449f5b6c58cd93e6548b1f7'
